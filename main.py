df2d7d33aab3a7ed17290bc328cc57957562a39f
Guilherme Morais <gui.moraes29@gmail.com>
2026-04-30 00:02:34 -0300

    JobExtractor && JobTransformer - CRIADOS

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import requests
 from pathlib import Path
 import json
@@ -42,21 +43,14 @@ class APIClient:
 
     def metodo_get(self, params):
         """funcao que faz a requisição get e recebe o objeto em json"""
-
         self.response = requests.get(url = self.endpoint, params= params)
         self.data = self.response.json()
         return self.data
 
-    def logger(self):
-        if self.response.status_code == 200:
-            log = {
-                "page_count": self.data["page_count"],
-                "items_per_page": self.data["items_per_page"],
-                "total": self.data["total"]
-            }
-            print(log)
-        else:
-            print(self.response.status_code)
+    def get_status_code(self):
+        if self.response is None:
+            return None
+        return self.response.status_code
 
 class JobExtractor:
     def __init__(self, qtd_paginas, dados):
@@ -64,22 +58,87 @@ class JobExtractor:
         self.dados = dados
         self.apiclient = APIClient(endpoint)
 
+        self.total_pages = 0
+        self.total_vagas = 0
+        self.logs = []
+
+    def logger(self):
+
+        self.status_code = self.apiclient.get_status_code()
+        try:
+            status = None
+            if self.status_code == 200:
+                status = "Sucesso"
+            else:
+                status = "Falhou"
+
+            log = {
+                "status_code": self.status_code,
+                "actual_page": self.get['page'],
+                "total_per_page": len(self.get['results']),
+                "status": status,
+            }
+        except Exception as e:
+            print(f"Ocorreu um erro: {e}")
+        else:
+            return log
+
     def paginacao_dados(self):
         """metodo que itera por pela quantidade de paginas definidas"""
         vagas = []
         for page in range(1, self.qtd_paginas +1):
+            #loop que faz a paginação
             self.dados['page'] = page
             time.sleep(1.5)
-            get = self.apiclient.metodo_get(self.dados)
-            for result in get['results']:
+            self.get = self.apiclient.metodo_get(self.dados)
+            self.total_pages +=1
+            self.logs.append(self.logger())
+            for result in self.get['results']:
+                self.total_vagas +=1
                 vagas.append(result)
-
         return vagas
+
+class JobTransformer:
+    def __init__(self, dados_brutos):
+        self.dados_brutos = dados_brutos
+
+
+    def convert_to_df(self):
+        df = pd.DataFrame(self.dados_brutos)
+        df['job_level'] = df['levels'].str[0].str.get('name') #metodo para acessar itens que estão em listas que contem dicts
+        df['company_id'] = df['company'].str.get('id') #metodo para acessar itens em dicts
+        df.to_csv('dados_brutos.csv', index=False, encoding='utf-8')
+
+        return df
+
+    def extract_and_join(self, lista_dict):
+        valores_extraidos = []
+        for dicionario in lista_dict:
+            for valor in dicionario.values():
+                valores_extraidos.append(str(valor))
+
+        return ' | '.join(valores_extraidos)
+
+    def create_new_df(self, df):
+        df_limpo = df.copy()
+        df_limpo['location_name'] = df_limpo['locations'].apply(self.extract_and_join)
+        df_limpo['company_name'] = df_limpo['company'].str.get('name')
+        df_limpo['link_vaga'] = df_limpo['refs'].str.get('landing_page')
+        df_limpo.rename(columns={'id':'job_id'}, inplace=True)
+        df_limpo = df_limpo.drop(['type','short_name','model_type','locations','levels', 'refs','company','tags'], axis=1)
+        df_limpo.insert(0,'job_id', df_limpo.pop('job_id'))
+
+        df_limpo.to_csv('dados_limpos.csv', index=False, encoding='utf-8')
+
+        return df_limpo
+
+
 
 
 params = coletar_dados(arquivo_key_api)
 salva_dados(params, arq_config)
-test = APIClient(endpoint)
 job = JobExtractor(5, params)
-job.paginacao_dados()
+
+test = JobTransformer(job.paginacao_dados())
+test.create_new_df(test.convert_to_df())
 
